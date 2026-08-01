b5bb984ff2473aaac8e5def56ee4ceebf4ef4480
Quentin Ågren <quentin.agren@gmail.com>
2026-08-01 07:51:52 +0200

    Initial attempt

---
+++ monitoring/rotate_apiserver_credentials.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+TOKEN=$(kubectl --context scilifelab-2-prod -n openllm create token --duration $((30*24))h monitoring)
+kubectl create secret generic -n loki-stack alloy-token-s2p \
+        --from-literal=OPENLLM_K8S_TOKEN="${TOKEN}" \
+        --from-literal=OPENLLM_K8S_APISERVER="${OPENLLM_K8S_APISERVER:-130.237.255.74:6443}" \
+        -o json --dry-run=client |
+  kubeseal --context scilifelab-1-dev -n loki-stack -w resources/token.ss.yaml
